5bcff7f137c52032a2d608a7f5f5d224ebd0123d
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2023-12-18 21:09:33 +0200

    Init project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.i192.praktika.programavimopraktika {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.almasb.fxgl.all;
+
+    opens com.i192.praktika.programavimopraktika to javafx.fxml;
+    exports com.i192.praktika.programavimopraktika;
+}
