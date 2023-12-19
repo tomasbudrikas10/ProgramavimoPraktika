56b812c5977bea2cc40a1a434157d8431987a8e4
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2023-12-19 11:27:52 +0200

    Sukurta pagrindinė aplikacija rodanti starto meniu

---
+++ src/main/java/com/i192/praktika/programavimopraktika/MainApplication.java
@@ -0,0 +1,27 @@
+package com.i192.praktika.programavimopraktika;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/i192/praktika/programavimopraktika/fxml/main-menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        scene.getStylesheets().add(getClass().getResource("/com/i192/praktika/programavimopraktika/css/style.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("Main Menu");
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
