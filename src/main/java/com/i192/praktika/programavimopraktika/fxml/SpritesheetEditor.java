fa72d00ac0b1075b409569ef0c8270918a1394b4
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2024-01-11 21:46:54 +0200

    sukurta spritesheet atidarymo funkcija

--- src/main/java/com/i192/praktika/programavimopraktika/fxml/SpritesheetEditor.java
+++ src/main/java/com/i192/praktika/programavimopraktika/fxml/SpritesheetEditor.java
@@ -1,38 +1,78 @@
 package com.i192.praktika.programavimopraktika.fxml;
 
 import com.i192.praktika.programavimopraktika.SceneManager;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class SpritesheetEditor implements Initialisable {
+    public ImageView openedSpritesheet;
+    public ScrollPane spritesheetScrollpane;
 
     @Override
     public void initialise() {
-
+        openedSpritesheet = new ImageView();
+        spritesheetScrollpane.setContent(openedSpritesheet);
     }
 
-    public void createOpenSpritesheetWindow(MouseEvent event) {
+    public Stage createOpenSpritesheetWindow(MouseEvent event) {
         Stage popupWindow = createPopup("Open Spritesheet", 300, 200, event);
         Button openFileSearchButton = new Button("Select Spritesheet");
+        Button setSpritesheetButton = new Button("Set Spritesheet");
         Pane popupContent = (Pane) popupWindow.getScene().getRoot();
-        popupContent.getChildren().add(openFileSearchButton);
+        VBox vbox = new VBox();
+        ImageView previewImage = new ImageView();
+        vbox.getChildren().add(openFileSearchButton);
+        vbox.getChildren().add(previewImage);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(20);
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select Spritesheet");
+        popupContent.getChildren().add(vbox);
         openFileSearchButton.setOnMouseClicked(e -> {
-            fileChooser.showOpenDialog(popupWindow);
+            // Show the file chooser dialog
+            File selectedFile = fileChooser.showOpenDialog(popupWindow);
+
+            if (selectedFile != null) {
+                // Check the file type based on its extension or MIME type
+                String fileName = selectedFile.getName();
+                String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+                if ("jpg".equalsIgnoreCase(fileExtension) ||
+                        "jpeg".equalsIgnoreCase(fileExtension) ||
+                        "png".equalsIgnoreCase(fileExtension)) {
+                    // Selected file is an image
+                    previewImage.setImage(new Image(selectedFile.toURI().toString()));
+                    previewImage.setPreserveRatio(true);
+                    previewImage.setFitWidth(200);
+                    vbox.getChildren().add(setSpritesheetButton);
+                }
+            }
         });
-        popupWindow.show();
-        System.out.println("Opened spritesheet");
+        setSpritesheetButton.setOnMouseClicked(e -> {
+            if (previewImage.getImage() != null) {
+                openedSpritesheet.setImage(previewImage.getImage());
+                popupWindow.close();
+            }
+        });
+
+        return popupWindow;
     }
 
-    public void openSpritesheet() {
-
+    public void openSpritesheet(MouseEvent event) {
+        createOpenSpritesheetWindow(event).show();
     }
 
     public void selectSpritesheet() {
